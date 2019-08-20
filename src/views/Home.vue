7477737a24ba0a525a5b85c202985e0d87938894
Lauryn <laurynpearson@gmail.com>
2019-08-19 20:39:10 -0500

    create index page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="student in students">
+      <h2> First Name: {{student.first_name}}</h2>
+      <h2> Last Name: {{student.last_name}}</h2>
+      <p> Email: {{student.email}}</p>
+      <p> Phone Number: {{student.phone_number}}</p>
+      <p> Bio: {{student.bio}}</p>
+      <p> Linkedin: {{student.linkedin}}</p>
+      <p> Twitter: {{student.twitter}}</p>
+      <p> Personal URL: {{student.personal_url}}</p>
+      <p> Resume URL: {{student.resume_url}}</p>
+      <p> GitHub: {{student.github_url}}</p>
+      <img v-bind:src="student.photo" v-bind:alt="student.first_name"/>
+      <hr>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from 'axios';
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to the index!",
+      students: []
+    };
+  },
+  created: function() {
+    axios.get('/api/students').then(response => {
+      console.log('in the index');
+      this.students = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
